f364a6d68acd6f3595a29d4086d06020882a656d
verhoek <30193551+verhoek@users.noreply.github.com>
2018-11-16 11:50:55 +0100

    Docker build deb script should now work without cd-ing to script path.

--- BuildTools/Installer/debian/docker-build-binary.sh
+++ BuildTools/Installer/debian/docker-build-binary.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-if [ ! -f "$1" ]; then
-	echo "Please provide the filename of an existing zip build as the first argument"
-	exit
-fi
+quit_on_error() {
+    echo "Error on line $1, stopping build of installer(s)."
+    exit 1
+}
 
-FILENAME=$(basename $1)
+set -eE
+trap 'quit_on_error $LINENO' ERR
+
+ZIPFILE=$1
+SCRIPT_DIR=$(dirname "$0")
+FILENAME=$(basename $ZIPFILE)
 DIRNAME=$(echo "${FILENAME}" | cut -d "_" -f 1)
 VERSION=$(echo "${DIRNAME}" | cut -d "-" -f 2)
 DATE_STAMP=$(LANG=C date -R)
 
-if [ -d "${DIRNAME}" ]; then
-	rm -rf "${DIRNAME}"
+
+if [ ! -f "$ZIPFILE" ]; then
+	echo "Please provide the filename of an existing zip build as the first argument"
+	exit
 fi
 
-unzip -d "${DIRNAME}" "$1"
+if [ -d "${SCRIPT_DIR}/${DIRNAME}" ]; then
+	rm -rf "${SCRIPT_DIR}/${DIRNAME}"
+fi
+
+unzip -d "${SCRIPT_DIR}/${DIRNAME}" "$1"
 
 for n in "../oem" "../../oem" "../../../oem"
 do
-    if [ -d $n ]; then
+    if [ -d "${SCRIPT_DIR}/$n" ]; then
         echo "Installing OEM files"
-        cp -R $n "${DIRNAME}/webroot/"
+        cp -R "${SCRIPT_DIR}/$n" "${SCRIPT_DIR}/${DIRNAME}/webroot/"
     fi
 done
 
@@ -28,29 +39,29 @@ for n in "oem-app-name.txt" "oem-update-url.txt" "oem-update-key.txt" "oem-updat
 do
     for p in "../$n" "../../$n" "../../../$n"
     do
-        if [ -f $p ]; then
+        if [ -f "${SCRIPT_DIR}/$p" ]; then
             echo "Installing OEM override file"
-            cp $p "${DIRNAME}"
+            cp "${SCRIPT_DIR}/$p" "${SCRIPT_DIR}/${DIRNAME}"
         fi
     done
 done
 
-cp -R "debian" "${DIRNAME}"
-cp "bin-rules.sh" "${DIRNAME}/debian/rules"
-sed -e "s;%VERSION%;$VERSION;g" -e "s;%DATE%;$DATE_STAMP;g" "debian/changelog" > "${DIRNAME}/debian/changelog"
+cp -R "${SCRIPT_DIR}/debian" "${SCRIPT_DIR}/${DIRNAME}"
+cp "${SCRIPT_DIR}/bin-rules.sh" "${SCRIPT_DIR}/${DIRNAME}/debian/rules"
+sed -e "s;%VERSION%;$VERSION;g" -e "s;%DATE%;$DATE_STAMP;g" "${SCRIPT_DIR}/debian/changelog" > "${SCRIPT_DIR}/${DIRNAME}/debian/changelog"
 
-touch "${DIRNAME}/releasenotes.txt"
+touch "${SCRIPT_DIR}/${DIRNAME}/releasenotes.txt"
 
-docker build -t "duplicati/debian-build:latest" - < Dockerfile.build
+docker build -t "duplicati/debian-build:latest" - < "${SCRIPT_DIR}/Dockerfile.build"
 
 # Weirdness with time not being synced in Docker instance
 sleep 5
 docker run  --workdir "/builddir/${DIRNAME}" --volume `pwd`:/builddir:rw "duplicati/debian-build:latest" dpkg-buildpackage
 
-rm -rf "${DIRNAME}"
-for filename in "duplicati_${VERSION}-1_amd64.changes" "duplicati_${VERSION}-1.dsc"  "duplicati_${VERSION}-1.tar.gz" 
+rm -rf "${SCRIPT_DIR}/${DIRNAME}"
+for filename in "duplicati_${VERSION}-1_amd64.changes" "duplicati_${VERSION}-1.dsc"  "duplicati_${VERSION}-1.tar.gz"
 do
-    if [ -f "${filename}" ]; then
-        rm "${filename}"
+    if [ -f "${SCRIPT_DIR}/${filename}" ]; then
+        rm "${SCRIPT_DIR}/${filename}"
     fi
 done
