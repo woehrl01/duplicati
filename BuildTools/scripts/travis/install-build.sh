7a2356da706bfe28535e9c303673344e4a218f7d
verhoek <30193551+verhoek@users.noreply.github.com>
2018-11-29 17:51:44 +0100

    Dockerizing.

--- BuildTools/scripts/travis/install-build.sh
+++ BuildTools/scripts/travis/install-build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt-get install apt-transport-https
-echo "deb https://download.mono-project.com/repo/ubuntu stable-trusty main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt-get update && \
-sudo apt-get install -y --no-install-recommends \
-        curl binutils curl mono-devel
+docker pull mono
