c84ab7d5c749d4eb200c8353b2f3371a509f84b6
verhoek <30193551+verhoek@users.noreply.github.com>
2018-12-04 08:42:18 +0100

    Fixed docker deploy install script.

--- BuildTools/scripts/travis/deploy/install.sh
+++ BuildTools/scripts/travis/deploy/install.sh
@@ -3,19 +3,23 @@
 SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 . "${SCRIPT_DIR}/../shared.sh"
 
-apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
+function install_docker () {
+   apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+   #    curl \
+   #    gnupg2 \
+      software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-apt-get update && apt-get install -y docker-ce
+   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+   apt-key fingerprint 0EBFCD88
+   add-apt-repository \
+      "deb [arch=amd64] https://download.docker.com/linux/debian \
+      $(lsb_release -cs) \
+      stable"
+   apt-get update && apt-get install -y docker-ce
+}
+
+install_docker
 
 #apt-get install -y git
